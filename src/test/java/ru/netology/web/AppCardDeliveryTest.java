a4a5291be95ae0b11b25e5269e5a5743b8ff4907
Nik test <nik.zyryanov.01@gmail.com>
2023-09-22 23:23:14 +0300

    Initial Commit

--- src/test/java/ru/netology/web/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/web/AppCardDeliveryTest.java
@@ -1,23 +1,15 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
-import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.DataGenerator;
-import sun.swing.SwingAccessor;
-
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static java.time.Duration.ofSeconds;
+
 
 
 public class AppCardDeliveryTest {
